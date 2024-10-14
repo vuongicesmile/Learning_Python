712468b4428cf6f5bff4c9364fdeaabb8e4cbc93
TITANCORP\vuong.nguyenq <nhut.nguyenv@titancorpvn.com>
2024-10-14 15:20:49 +0700

    Cau lenh if

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-ho_ten = input("Ten cua ban la gi: ")
-print("Xin chao " + ho_ten)
+is_cold=True
+is_hot=True
+if is_cold:
+    print("Troi lanh")
+elif is_hot:
+    print("troi nong")
+    # neu khong thi bam shift Tab de ra dau dong
+print("chuc ban vui ve")
