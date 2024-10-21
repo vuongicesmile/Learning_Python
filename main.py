298888657a7d4337d4612fc94a1d57301ae1a6c3
TITANCORP\vuong.nguyenq <nhut.nguyenv@titancorpvn.com>
2024-10-21 14:58:37 +0700

    Su dung function trong python

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# khong the thay doi phan tu trong tuples
+def chao_mung(name):
+    print(f"chao mung ban {name} den voi python")
+    print(f"chuc ban {name} thanh cong")
 
-day_so = (1,2,3,4,5,6)
-# thay doi dau [] thay dau ()
-# khi muon thao tac voi danh sach ma ko muon thay doi danh sach do
-
-#ky thuat unpacking
-day_so_test = (1,2,3)
-tich = day_so_test[0] * day_so_test[1] * day_so_test[0]
-
-# => ky thuat unpacking
-
-x,y,z = day_so_test
-tich1 = x * y * z
-print(tich1)
-
-
+ # cach 2 dong de bat dau
+print("bat dau")
+chao_mung("vuong")
+print("ket thuc")
 
