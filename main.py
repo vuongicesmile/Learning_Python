d433f2474eae99a18d71325eca0ea50dfb92d6b6
TITANCORP\vuong.nguyenq <nhut.nguyenv@titancorpvn.com>
2024-10-21 13:59:41 +0700

    Kieu du lieu tuple va ky thuat unpacking

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-name = [1, 2,3]
+# khong the thay doi phan tu trong tuples
 
-max=name[0]
+day_so = (1,2,3,4,5,6)
+# thay doi dau [] thay dau ()
+# khi muon thao tac voi danh sach ma ko muon thay doi danh sach do
 
-# duyet qua tu dau den cuoi , nao lon hon thi gan
-for i in name:
-    if i > max:
-        max=i
-print(max)
+#ky thuat unpacking
+day_so_test = (1,2,3)
+tich = day_so_test[0] * day_so_test[1] * day_so_test[0]
+
+# => ky thuat unpacking
+
+x,y,z = day_so_test
+tich1 = x * y * z
+print(tich1)
+
 
 
